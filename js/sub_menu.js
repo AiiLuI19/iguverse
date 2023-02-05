9da5ef5ee09c0f35407a1e589206b0eb69db2af2
Юлія <iuliiaiavorskaia@gmail.com>
2023-02-06 01:20:27 +0200

    add js + fix bag

--- js/sub_menu.js
+++ js/sub_menu.js
@@ -1,45 +1,30 @@
-(() => {
+
     const refs = {
       openMenuBtn: document.querySelector(".dropdown-toggle"),
-      closeMenuBtn: document.querySelector(".menu-close-btn"),
       subMenu: document.querySelector(".dropdown_menu"),
-      body:document.querySelector("#body"),
-      
     };
   
     refs.openMenuBtn.addEventListener("click", toggleSubMenu);
-    refs.closeMenuBtn.addEventListener("click", toggleSubMenu);
-    
-    
-  
-    function toggleSubMenu() {
-        event.preventDefault();
-      const open= refs.subMenu.classList.toggle("is-hidden");
-    // const open= refs.subMenu.classList.remove("is-hidden");
-      console.log(open)
-    //   if (open){
-    //     body.addEventListener("click",(e)=>{
-    //         if(e.target===refs.subMenu){
-    //             return;
-    //         } 
-    //         refs.subMenu.classList.add("is-hidden");
-    //     })
-    //   }
-    }
-    
-   
-  })();
-//   const dropdown= document.querySelector(".is-hidden");
-//     document.addEventListener("click", (e) => {
-//         const withinBoundaries = e.composedPath().includes(dropdown);
-//      console.log(withinBoundaries);
-//         if ( withinBoundaries ) {
-//           console.log("ok");
-//             dropdown.style.display = 'none'; // скрываем элемент т к клик был за его пределами
-//         }
-//     });
+    function toggleSubMenu(e) {
+      e.preventDefault();
+      
+      window.addEventListener("click",dropdownClose);
+      return }
 
+ 
+       function dropdownClose(){
+          if(refs.subMenu.classList.contains("is-hidden")){
+            refs.subMenu.classList.remove("is-hidden");
+           
+               return;
+              }
+         
+         refs.subMenu.classList.add("is-hidden"); 
+          window.removeEventListener("click",dropdownClose);
+        };
+      
   
+ 
 
 
   
