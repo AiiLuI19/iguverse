b4db8395ec7d798384765cba61c7bb78ec8620db
Юлія <iuliiaiavorskaia@gmail.com>
2023-02-05 17:09:40 +0200

    fix js

--- js/sub_menu.js
+++ js/sub_menu.js
@@ -2,13 +2,43 @@
     const refs = {
       openMenuBtn: document.querySelector(".dropdown-toggle"),
       closeMenuBtn: document.querySelector(".menu-close-btn"),
-      subMenu: document.querySelector(".dropdown-menu"),
+      subMenu: document.querySelector(".dropdown_menu"),
+      body:document.querySelector("#body"),
+      
     };
   
     refs.openMenuBtn.addEventListener("click", toggleSubMenu);
     refs.closeMenuBtn.addEventListener("click", toggleSubMenu);
+    
+    
   
     function toggleSubMenu() {
-      refs.subMenu.classList.toggle("is-hidden");
+      const open= refs.subMenu.classList.toggle("is-hidden");
+    // const open= refs.subMenu.classList.remove("is-hidden");
+      console.log(open)
+    //   if (open){
+    //     body.addEventListener("click",(e)=>{
+    //         if(e.target===refs.subMenu){
+    //             return;
+    //         } 
+    //         refs.subMenu.classList.add("is-hidden");
+    //     })
+    //   }
     }
+    
+   
   })();
+//   const dropdown= document.querySelector(".is-hidden");
+//     document.addEventListener("click", (e) => {
+//         const withinBoundaries = e.composedPath().includes(dropdown);
+//      console.log(withinBoundaries);
+//         if ( withinBoundaries ) {
+//           console.log("ok");
+//             dropdown.style.display = 'none'; // скрываем элемент т к клик был за его пределами
+//         }
+//     });
+
+  
+
+
+  
